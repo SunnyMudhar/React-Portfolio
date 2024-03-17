64f9b0e5e8c5c2c5b1c2d388d707ace6dccc857f
Sunny <sunielmudhar@gmail.com>
2024-03-17 04:21:46 +0000

    Trying relative file paths with CV for netlify

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,3 +1,4 @@
+import Cv from '../../assets/documents/SunielMudhar_CV.pdf'
 import Container from '../Container';
 import Row from '../Row'
 import './style.css'
@@ -17,7 +18,7 @@ const About = () => {
                     <p className='cv-text'>
                         Want to know more about my professional work history? Take a look at my CV.
                     </p>
-                    <a href="src/assets/documents/SunielMudhar_CV.pdf" download="SunielMudhar_CV.pdf"><button type="button" class="btn btn-primary btn-download">Download CV</button></a>
+                    <a href={ Cv } download="SunielMudhar_CV.pdf"><button type="button" class="btn btn-primary btn-download">Download CV</button></a>
                 </div>
             </Row>
         </Container>
